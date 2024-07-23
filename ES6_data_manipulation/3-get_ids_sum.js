52cfe7f665437ebae4ec5ca2744e0013f25f5056
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-07-23 12:21:33 +0400

    3-get_ids_sum.js

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-export default function getStudetIdsSum(students) {
-        return students.reduce((sum, student) => sum + students.id, 0);
+export default function getStudentIdsSum(students) {
+	return students.reduce((sum, student) => sum + student.id, 0);
 }
