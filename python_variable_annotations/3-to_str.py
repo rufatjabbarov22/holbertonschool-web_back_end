a12d1bba4af5b45f84929185b89a575d4cff84bc
Rufat Jabbarov <rufatjabbarov54@gmail.com>
2024-08-17 20:06:32 +0400

    task-3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""to_str function with type annotations."""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The float to be converted to a string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
