ec82e674a3bb7a34b1fcbec7417e30fb1c8b3560
Rufat Jabbarov <rufatjabbarov54@gmail.com>
2024-08-17 20:05:16 +0400

    task-2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""floor function with type annotations."""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float.
+
+    Args:
+        n (float): The float to be floored.
+
+    Returns:
+        int: The floored number.
+    """
+    return int(n)
