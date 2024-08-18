e317fddd42df9e191248808bbeeb167e0480460f
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-18 14:37:12 +0400

    0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""Basic async syntax."""
-import random
+"""
+Random and asyncio modules are imported
+"""
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random amount of seconds."""
-    delay = random.random() * max_delay
-    await asyncio.sleep(delay)
-    return delay
+    """
+    Function waits until random float time between 0 and max_delay
+    """
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
     
