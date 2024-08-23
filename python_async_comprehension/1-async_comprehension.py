afc319199a2db0bdc33b80e6269c6377c18dfae8
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-23 15:01:50 +0400

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""Module for async comprehension."""
+"""n write a coroutine called async_comprehension
+that takes no arguments."""
 import asyncio
+import random
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Module returning async collected list"""
-    return [i async for i in async_generator()]
-  
+    """he coroutine will collect 10 random numbers
+    using an async comprehensing over async_generator,
+    then return the 10 random numbers."""
+    return [value async for value in async_generator()]
