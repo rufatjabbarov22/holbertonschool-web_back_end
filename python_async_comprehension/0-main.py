c67be47eb9c39fa28375d28a8bd74048944d120f
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-23 15:00:53 +0400

    0-main.py

---
+++ python_async_comprehension/0-main.py
@@ -0,0 +1,8 @@
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
