3c4566b877a32b4ee1144b910140668634ab8620
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-24 16:52:05 +0400

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
-"""
-Checking Nginx stats
-"""
-import pymongo
+""" 12-log_stats.py """
+
+from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    Logs...
-    """
-    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    doc_count = collection.estimated_document_count()
-    print("{} logs".format(doc_count))
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        method_count = collection.count_documents({'method': method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status_count = collection.count_documents(
-                {'method': 'GET', 'path': "/status"})
-    print("{} status check".format(status_count))
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    co = client.logs.nginx
 
+    methods = {
+        'GET': 0,
+        'POST': 0,
+        'PUT': 0,
+        'PATCH': 0,
+        'DELETE': 0
+    }
+    status_count = 0
+    for v in co.find():
+        if 'method' in v:
+            if v['method'] in methods:
+                methods[v['method']] = methods[v['method']] + 1
+
+            if v['method'] == 'GET' and 'path' in v and v['path'] == '/status':
+                status_count = status_count + 1
+
+    print("{0} logs".format(co.count_documents({})))
+    print("Methods:")
+    print("\tmethod GET: {0}".format(methods['GET']))
+    print("\tmethod POST: {0}".format(methods['POST']))
+    print("\tmethod PUT: {0}".format(methods['PUT']))
+    print("\tmethod PATCH: {0}".format(methods['PATCH']))
+    print("\tmethod DELETE: {0}".format(methods['DELETE']))
+    print("{0} status check".format(status_count))
