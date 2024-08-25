82dcd989ddd664b4c32267aec6e8dbe2f15e5ac4
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-08-25 14:29:00 +0400

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
-"""Print info f nginx logs """
-from pymongo import MongoClient
+"""
+Checking Nginx stats
+"""
+import pymongo
+
 
 if __name__ == "__main__":
-    """ check for all elements in a collection """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    """
+    Logs...
+    """
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-
-    print(f"{collection.estimated_document_count()} logs")
-
+    doc_count = collection.estimated_document_count()
+    print("{} logs".format(doc_count))
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
         method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_count}")
-
-    check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
-    print(f"{check_get} status check")
+        print("\tmethod {}: {}".format(method, method_count))
+    status_count = collection.count_documents(
+                {'method': 'GET', 'path': "/status"})
+    print("{} status check".format(status_count)) 
