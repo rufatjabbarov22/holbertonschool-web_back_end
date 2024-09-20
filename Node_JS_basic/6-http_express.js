c5a5a4dbc1dc44fcd7e695adb50bce97024f99d0
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-09-20 19:55:32 +0400

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,12 +1,17 @@
-// 6-http_express.js
 const express = require('express');
 
+// Créez une application Express
 const app = express();
 
+// Définissez une route pour l'endpoint /
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
-module.exports = app;
+// Faites écouter le serveur sur le port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
+// Exportez l'application Express
+module.exports = app;
