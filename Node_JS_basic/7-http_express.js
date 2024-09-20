d5c38441a9dda180e2662553b3cf5b965057af70
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-09-20 19:58:26 +0400

    f42

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-const port = 1245;
 const app = express();
-module.exports = app;
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-  countStudents(database).then((data) => {
-    res.end(data.join('\n'));
-  }).catch((error) => {
-    res.end(`${error.message}`);
-  });
+
+  const databasePath = process.argv[2];
+  countStudents(databasePath)
+    .then((result) => {
+      res.end(result);
+    })
+    .catch((error) => {
+      res.end(error.message);
+    });
 });
 
-app.listen(port);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
