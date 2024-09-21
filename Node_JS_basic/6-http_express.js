7b628b376589257b52d8f276873658c1ba0ac53b
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-09-21 14:31:26 +0400

    6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,11 +1,32 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 
-const port = 1245;
+const port = process.env.PORT || 1245;
 const app = express();
-module.exports = app;
+
+// Логирование запросов
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port);
+// Обработка ошибок 404
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+});
+
+// Общая обработка ошибок
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Запускаем сервер
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Экспортируем приложение
+module.exports = app;
